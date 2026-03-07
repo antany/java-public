8e72717d062ef538a93c9361cfab21677d99377a
antany <antany84@gmail.com>
2026-03-07 10:34:58 -0500

    Add terminal art script for church theme

---
+++ terminal-art-chruch.sh
@@ -0,0 +1,25 @@
+# Define colors using tput
+GOLD=$(tput setaf 220)
+GREY=$(tput setaf 252)
+L_GREY=$(tput setaf 250)
+BLUE=$(tput setaf 117)
+LAVENDER=$(tput setaf 141)
+CREAM=$(tput setaf 229)
+WHITE=$(tput setaf 15)
+RESET=$(tput sgr0)
+
+echo ""
+echo "               ${GOLD}+${RESET}"
+echo "              ${GREY}/ \\${RESET}"
+echo "             ${GREY}/   \\${RESET}"
+echo "            ${GREY}/_____\\${RESET}"
+echo "            ${GREY}|  ${BLUE}_${GREY}  |${RESET}"
+echo "            ${GREY}| ${BLUE}(_)${GREY} |${RESET}"
+echo "    ${L_GREY}_  _    ${GREY}|_____|${L_GREY}    _  _${RESET}"
+echo "   ${L_GREY}| || |  ${GREY}/       \\${L_GREY}  | || |${RESET}"
+echo "   ${L_GREY}| || | ${GREY}|  ${LAVENDER}░${CREAM}[ ]${LAVENDER}░${GREAM}${RESET}  | ${L_GREY}| || |${RESET}"
+echo "   ${L_GREY}| || | ${GREY}|  ${LAVENDER}░${CREAM}[ ]${LAVENDER}░${GREAM}${RESET}  | ${L_GREY}| || |${RESET}"
+echo "   ${L_GREY}|____| ${GREY}|___${CREAM}|_|${GREY}___| ${L_GREY}|____|${RESET}"
+echo ""
+echo "      ${WHITE}P R A I S E   J E S U S${RESET}"
+echo ""
